10c0ce9428e7dfbe283c3daded5ba9c588842fc0
brianlen <brianlen@umich.edu>
2024-04-21 09:31:31 -0600

    added 8 components for the pages

---
+++ frontend/src/components/AssessmentDetails.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AssessmentDetails() {
+  return (
+    <div>
+      <h1>Assessment Details</h1>
+    </div>
+  );
+}
---
+++ frontend/src/components/CreateAccountLogin.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CreateAccountLogin() {
+  return (
+    <div>
+      <h1>Create Account/Login</h1>
+    </div>
+  );
+}
