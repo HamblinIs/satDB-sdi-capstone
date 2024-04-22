db856d56390028f462c45bbe1657b43fd2c9f2a0
Isaac Hamblin <isaac.hamblin@gmail.com>
2024-04-22 10:16:40 -0600

    added cors

--- api-server/api.js
+++ api-server/api.js
@@ -1,7 +1,10 @@
 const express = require("express");
-
+const cors = require('cors');
 const api = express();
 const port = process.env.PORT || 8080;
+const knex = require('knex')(require('./knexfile.js')['development'])
+
+app.use(cors(), express.json())
 
 api.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
