54229bbcb5b5247d1e07dac63c989cce95fcc87a
Deven <hudde13@gmail.com>
2024-04-24 10:31:18 -0600

    wed morning HG push

--- frontend/src/components/CreateAssessment.js
+++ frontend/src/components/CreateAssessment.js
@@ -7,7 +7,7 @@ export default function CreateAssessment() {
   const[satellite, setSatellite] = useState('');
   const [assessment, setAssessment] = useState({
     name: '',
-    // satellites: [],
+    associatedSatTailNum: 0,
     creation_date:'',
     description: '',
     // owner: '',
@@ -43,23 +43,13 @@ export default function CreateAssessment() {
 
         body: JSON.stringify({
           name: assessment.name,
+          associatedSatTailNum: assessment.associatedSatTailNum,
           creation_date: assessment.creation_date,
           description: assessment.description
         })
       })
-        // .then(response => {
-        //   console.log(response)
-        // })
-        // .then(async (res) => {
-        //   if (res.status === 201) {
-        //     window.confirm("Assmen has been created!")
-        //     // navigate(`/AssessmentDetails/${res.id}`)
-        //   } else {
-        //     alert("There was an error creating the assessment");
-        //   }
-        // })
-        .then(res => res.json())
-        .then(res => navigate(`/AssessmentDetails/${res.id}`));
+      .then(res => res.json())
+      .then(res => navigate(`/AssessmentDetails/${res.id}`));
     } catch (error) {
       console.error('Failed to add assessment:', error);
     }
@@ -102,32 +92,25 @@ export default function CreateAssessment() {
   return (
     <div>
       <h1>Create Assessment</h1>
-
       <>
-      
-      <label>Name:
+        <label>Name:
           <input type="text" name="name" value={assessment.name} onChange={handleChange} />
         </label>
         <br/>
-
-      <label>Associated Satellite:
-        <input type="text" name="associatedSat" value={assessment.associatedSat} onChange={handleChange} />
-      </label>
-      <br/>
-      <label>Date:
+        <label>Associated Satellite:
+          <input type="number" name="associatedSatTailNum" value={assessment.associatedSatTailNum} onChange={handleChange} />
+        </label>
+        <br/>
+        <label>Date:
           <input type="date" name="creation_date" value={assessment.creation_date} onChange={handleChange} />
         </label>
         <br/>
-      <label>Description:
+        <label>Description:
           <textarea name="description" value={assessment.description} onChange={handleChange} />
         </label>
         <br/>
-      <label>Owner:
-          <input type="text" name="owner" value={assessment.owner} onChange={handleChange} />
-        </label>
-        <br/>
         <label># of Model Files
-        <select id = "modelFilesNum" name="modelFilesNum"  onChange={() => setModelFilesNum()}>
+          <select id = "modelFilesNum" name="modelFilesNum"  onChange={() => setModelFilesNum()}>
             <option value="">--Please choose an option--</option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -140,22 +123,22 @@ export default function CreateAssessment() {
             <option value="9">9</option>
             <option value="10">10</option>
           </select>
-          </label>
-          <br />
-          {modFiles === undefined ?
+        </label>
+        <br />
+        {modFiles === undefined ?
           <></>
           :
           <>
-          <label>Model File:
-          <input type="file" name="model_file" onChange={handleFileChange} />
-        </label>
-        <br/>
-          {modFiles.map( item =>  item)}
-        <br/>
-        </>
+            <label>Model File:
+              <input type="file" name="model_file" onChange={handleFileChange} />
+            </label>
+            <br/>
+            {modFiles.map( item =>  item)}
+            <br/>
+          </>
         }
         <label># of Simulation Files
-        <select id = "simFilesNum" name="simFilesNum"  onChange={() => setSimFilesNum()}>
+          <select id = "simFilesNum" name="simFilesNum"  onChange={() => setSimFilesNum()}>
             <option value="">--Please choose an option--</option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -168,24 +151,22 @@ export default function CreateAssessment() {
             <option value="9">9</option>
             <option value="10">10</option>
           </select>
-          </label>
-          <br />
-          {simFiles === undefined ?
+        </label>
+        <br />
+        {simFiles === undefined ?
           <></>
           :
           <>
-          <label>Simulation File:
-          <input type="file" name="model_file" onChange={handleFileChange} />
-        </label>
-        <br/>
-          {simFiles.map( item =>  item)}
-        <br/>
-        </>
+            <label>Simulation File:
+              <input type="file" name="model_file" onChange={handleFileChange} />
+            </label>
+            <br/>
+            {simFiles.map( item =>  item)}
+            <br/>
+          </>
         }
-
         <button onClick = {() => handleSubmit()}>Submit</button>
-
-    </>
+      </>
     </div>
   );
 }
