0c5e4fe93f13d42a02d647934be77f854b510c74
Deven <hudde13@gmail.com>
2024-04-24 15:37:42 -0600

    assessment creator 90% completed

--- frontend/src/components/CreateAssessment.js
+++ frontend/src/components/CreateAssessment.js
@@ -1,51 +1,57 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function CreateAssessment() {
   const navigate = useNavigate();
   const[satellite, setSatellite] = useState('');
+  const [associatedSatList, setAssociatedSatList] = useState([]);
   const [assessment, setAssessment] = useState({
     name: '',
-    associatedSatTailNum: 0,
+    associatedSat: 0,
     creation_date:'',
     description: '',
-    // owner: '',
-    // model_file: '', // todo: need to have an input for model files and simulation files
-    // simulation_file: '',
-    // misc_files: []
+    model_file: '',
+    simulation_file: '',
+    misc_files: ''
   });
-  const [modFiles, setModFiles] = useState();
-  const [simFiles, setSimFiles] = useState();
-  const modelCounter = [];
-  const simCounter = [];
 
   const handleChange = (e) => {
-    // name is the key name
-    // value is e.target.value
     const { name, value } = e.target;
     setAssessment(prevState => ({ ...prevState, [name]: value }));
   };
 
-  const handleFileChange = (e) => {
-    // setAssessment(prevState => ({ ...prevState, file: e.target.files[0] }));
-  };
+  useEffect(() => {
+  const satList = async () =>{
+    try {
+      await fetch('http://localhost:8080/satellites?name')
+      .then(response => response.json())
+      .then(data => {
+        setAssociatedSatList(data)
+      })
+    } catch (error) {
+      console.error('Failed to add assessment:', error);
+    }
+  }
+  satList();
+  }, [])
 
   const handleSubmit = () => {
     // e.preventDefault();
-
     try {
       fetch('http://localhost:8080/assessment/new', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-
         body: JSON.stringify({
           name: assessment.name,
-          associatedSatTailNum: assessment.associatedSatTailNum,
+          associatedSat: assessment.associatedSat,
           creation_date: assessment.creation_date,
-          description: assessment.description
+          description: assessment.description,
+          model_file: assessment.model_file,
+          simulation_file: assessment.simulation_file,
+          misc_files: assessment.misc_files
         })
       })
       .then(res => res.json())
@@ -53,40 +59,6 @@ export default function CreateAssessment() {
     } catch (error) {
       console.error('Failed to add assessment:', error);
     }
-  }
-
-
-  const setModelFilesNum = () =>{
-  //     const amount = document.getElementById("modelFilesNum").value
-
-  //     for(let i=1; i<amount; i++){
-  //       modelCounter.push(<>
-  //         <label>Model File:
-  //           <input type="file" name="model_file" onChange={handleFileChange} />
-  //         </label>
-  //         <br/>
-  //         </>)
-  //     }
-  //     console.log("counter", modelCounter)
-  //     setModFiles(modelCounter)
-  }
-
-  const setSimFilesNum = () => {
-  //   const amount = document.getElementById("simFilesNum").value
-
-
-  //   for(let i=1; i<amount; i++ ){
-  //     simCounter.push(
-  //       <>
-  //       <label>Simulation File:
-  //           <input type="file" name="simulation_file" onChange={handleFileChange} />
-  //         </label>
-  //         <br/>
-  //       </>
-  //     )
-  //   }
-  //   console.log("counter", simCounter)
-  //   setSimFiles(simCounter)
   }
 
   return (
@@ -97,74 +69,43 @@ export default function CreateAssessment() {
           <input type="text" name="name" value={assessment.name} onChange={handleChange} />
         </label>
         <br/>
+
         <label>Associated Satellite:
-          <input type="number" name="associatedSatTailNum" value={assessment.associatedSatTailNum} onChange={handleChange} />
+          <select id="associatedSat" name="associatedSat" value={assessment.associatedSat}  onChange={handleChange}>
+            <option value="">--Please choose an option--</option>
+            {associatedSatList.map(sat =>
+              <option key={sat.tail_num} value={sat.id}>{`${sat.name}, Tail #: ${sat.tail_num}`}</option>
+            )}
+              <option value="N/A">No Associated Satellites</option>
+          </select>
         </label>
         <br/>
+
         <label>Date:
           <input type="date" name="creation_date" value={assessment.creation_date} onChange={handleChange} />
         </label>
         <br/>
+
         <label>Description:
           <textarea name="description" value={assessment.description} onChange={handleChange} />
         </label>
         <br/>
-        <label># of Model Files
-          <select id = "modelFilesNum" name="modelFilesNum"  onChange={() => setModelFilesNum()}>
-            <option value="">--Please choose an option--</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
+
+        <label>Model File:
+          <input type="text" name="model_file" value={assessment.model_file} onChange={handleChange} />
         </label>
-        <br />
-        {modFiles === undefined ?
-          <></>
-          :
-          <>
-            <label>Model File:
-              <input type="file" name="model_file" onChange={handleFileChange} />
-            </label>
-            <br/>
-            {modFiles.map( item =>  item)}
-            <br/>
-          </>
-        }
-        <label># of Simulation Files
-          <select id = "simFilesNum" name="simFilesNum"  onChange={() => setSimFilesNum()}>
-            <option value="">--Please choose an option--</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
+        <br/>
+
+        <label>Simulation File:
+        <input type="text" name="simulation_file" value={assessment.simulation_file} onChange={handleChange} />
         </label>
-        <br />
-        {simFiles === undefined ?
-          <></>
-          :
-          <>
-            <label>Simulation File:
-              <input type="file" name="model_file" onChange={handleFileChange} />
-            </label>
-            <br/>
-            {simFiles.map( item =>  item)}
-            <br/>
-          </>
-        }
+        <br/>
+
+        <label>Misc File:
+        <input type="text" name="misc_files" value={assessment.misc_files} onChange={handleChange} />
+        </label>
+        <br/>
+
         <button onClick = {() => handleSubmit()}>Submit</button>
       </>
     </div>
