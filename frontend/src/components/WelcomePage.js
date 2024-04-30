3af6ca309b549285787b6822b82744a50a479e3c
Avery <evans.c.avery@gmail.com>
2024-04-30 09:45:27 -0600

    misc commit

--- frontend/src/components/WelcomePage.js
+++ frontend/src/components/WelcomePage.js
@@ -56,14 +56,14 @@ const ULDiv = styled.ul`
   justify-content: left;
   align-items: left;
   align-content: left;
-  list-style: circle;
+  list-style: square;
   li {
     justify-items: left;
     justify-content: left;
     align-items: left;
     align-content: left;
     margin-bottom: 8px;
-  }
+  };
 `
 
 const imagesArr = ["https://cdn.defenseone.com/media/img/cd/2023/08/11/GettyImages_1407240226/open-graph.jpg", "https://spaceplace.nasa.gov/satellite/en/TEMPO.en.jpg", "https://media.istockphoto.com/id/1339097795/photo/satellite-orbiting-the-earth.jpg?s=612x612&w=0&k=20&c=FMG2NypIT0JuZVs26qSYOq2qTwsO89woydrwZimK21s="];
