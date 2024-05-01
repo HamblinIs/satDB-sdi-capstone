983759a5222b7f0ffc49fe478901f85fa80646bb
Deven <hudde13@gmail.com>
2024-05-01 09:24:02 -0600

    full merge to main

--- frontend/src/components/NewNav/Nav.jsx
+++ frontend/src/components/NewNav/Nav.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
 import './Nav.css'
+import { Link, useMatch, useResolvedPath, useNavigate } from 'react-router-dom';
+import { IoIosArrowDown, IoIosArrowUp, IoIosMenu } from "react-icons/io";
+// import styled from 'styled-components'
+
 import { TfiMenu } from "react-icons/tfi"
 
+// const DropdownContent = styled.div`
+//   display: ${props => (props.isOpen ? 'block' : 'none')};
+//   position: absolute;
+//   background-color: #f9f9f9;
+//   min-width: 160px;
+//   border: 1px solid black;
+//   z-index: 1;
+// `;
+
 export default function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
+
   return (
     <div className='navbar'>
-      <TfiMenu className='hamburger' />
+      <TfiMenu className='hamburger'/>
       <div className='linkcontainer'>
-          <p>Create</p>
+            <button className='button3' onMouseOver={handleToggle} onMouseLeave={handleMouseLeave}>
+              Create {isOpen ? <IoIosArrowUp/> : <IoIosArrowDown/>}
+            </button>
+            <div isOpen={isOpen}>
+              <Link to="/CreateAssessment" onClick={handleLinkClick}>Create Assessment</Link>
+              <Link to="/CreateSatellite" onClick={handleLinkClick}>Create Satellite</Link>
+            </div>
           <p>View</p>
           <p>Ground-Track</p>
           <p>Satellite Orbit</p>
--- frontend/src/routes/MainPage.jsx
+++ frontend/src/routes/MainPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Nav  from '../components/NewNav/Nav';
 import Login from '../components/NewLogin/Login';
+import NewCreateSatellite from '../components/NewCreateSatellites/CreateNewSatellite';
+
 
 export default function MainPage() {
   return (
     <div>
       <Nav/>
+
+      <NewCreateSatellite/>
     </div>
   )
 }
