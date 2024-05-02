bd56096f82e868014ed130557e8dc27910a64d15
SPC3-Valle <jakevalle99@gmail.com>
2024-05-02 11:56:09 -0400

    trying to fix

--- frontend/src/Create.test.js
+++ frontend/src/Create.test.js
@@ -12,6 +12,38 @@ test('homepage text should display', () => {
   expect(titleText).toBeInTheDocument()
 })
 
+const server = setupServer(...createHandlers)
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+
+test('should navigate to Login Screen', async() => {
+  render(<App />);
+  const lTab = screen.getByText("Login/Register");
+  await act(async() => {userEvent.click(lTab)})
+  let lBut = screen.getByText("Login")
+  await act(async() => {userEvent.click(lBut)})
+  let logBoxes = screen.getAllByRole('textbox')
+  await act(async() => {userEvent.click(screen.getByText("Email:"))})
+  userEvent.keyboard('ihamblin@yahoo.com')
+  await act(async() => {userEvent.click(screen.getByText("Password:"))})
+  userEvent.keyboard('Password123')
+  // let eEnter = document.getElementById("username")
+  // await act(async() => {userEvent.type(eEnter, 'ihamblin@yahoo.com')})
+  // let passEnter = document.getElementById("password")
+  // await act(async() => {userEvent.type(passEnter, 'Password123')})
+  //let tes = screen.getByText("ihamblin@yahoo.com")
+  let subButton = screen.getByRole('button', {name: "Submit"})
+  await act(async() => {userEvent.click(subButton)})
+  await waitFor(() => screen.getByText("Logout"))
+  expect(lBut).toBeInTheDocument()
+  //expect(logButt).toBeInTheDocument()
+  //expect(passEnter).toBeInTheDocument()
+  expect(logBoxes.length).toEqual(2)
+}),
+
 test('should navigate to the Create Satel', async() => {
     render(<App />);
     const cTab = screen.getByText("Create");
@@ -21,13 +53,7 @@ test('should navigate to the Create Satel', async() => {
     await waitFor(() => screen.getByText('Model File:'));
     let talTab = screen.getByText('Tail Number:')
     expect(talTab).toBeInTheDocument()
-  })
-
-const server = setupServer(...createHandlers)
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+  }),
 
 test('submitting a satellite will take user to details page', async() => {
     render(<App />);
